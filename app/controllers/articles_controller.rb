5d03a48e34e17137005aa21284adb248c6af53bf
stberg3 <stberg3@gmail.com>
2017-06-30 14:43:56 -0500

    Now we're [here](http://guides.rubyonrails.org/getting_started.html#adding-links)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,11 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 
   def create
